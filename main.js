709838d1190f09b3419c65f14f78c3b811aa1738
Nathandiv <k.n.hlungwani@gmail.com>
2024-08-20 09:15:22 +0200

    completed my back end script

--- main.js
+++ main.js
@@ -1,52 +1,98 @@
-let mysql = require ('mysql2')
-const express = require ('express');
-const { Server } = require('http');
-const app = express()
-app.use(express.json())
-
-
+let mysql = require("mysql2");
+const express = require("express");
+const { Server } = require("http");
+const app = express();
+app.use(express.json());
 
 const connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password :'Nathandata',
-    database : 'Empolyess'
-})
-
+  host: "localhost",
+  user: "root",
+  password: "Nathandata",
+  database: "Empolyess",
+});
 
 connection.connect((err) => {
-    if (err){
-        console.log(err);
-    }
-    else{
-        console.log('connected');
-    }
-    })
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connected");
+  }
+});
 
-    app.post('/post', (req, res)=>{
-        const {id, name, employee_code, salary} = req.body
+app.post("/post", (req, res) => {
+  const { id, name, employee_code, salary } = req.body;
+
+  if (!name || !employee_code || !salary) {
+    return res.json("Enter valid details");
+  }
+
+  connection
+    .promise()
+    .query("insert into empolyee_details values (?,?,?,?)", [
+      id,
+      name,
+      employee_code,
+      salary,
+    ])
+    .then((data) => res.status(200).send(data[0]))
+    .catch((err) => console.log(err));
+});
+
+app.get("/get", (req, res) => {
+  connection
+    .promise()
+    .query("SELECT * FROM empolyee_details")
+    .then((data) => res.send(data[0]))
+    .catch((err) => console.log(err));
+});
+
+app.get("/get/:id", (req, res) => {
+  const items = req.params.id;
+  connection.query(
+    "SELECT * FROM empolyee_details WHERE id = ?",
+    items,
+    (err, result) => {
+      if (err) {
+        console.log("error", err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.put('/put/:id', (req,res) =>{
+    const id = req.params.id
+    const { name, employee_code, salary} = req.body
+
+ connection.query('UPDATE empolyee_details SET employee_code = ? WHERE id = ?', [employee_code,id], (err)=>{
+    if(err){
+        console.log('error', err);
+    }else
+    {
+        res.send('update')
+    }
+ })
+})
+
+app.delete('/delete/:id', (req,res)=>{
+    const del = req.params.id
+    connection.query('DELETE FROM empolyee_details WHERE id = ?', del,(err)=>{
     
-        if ( !name || !employee_code ||  !salary){
-            return res.json('Enter valid details')
+        if(err){
+            console.log('error', err);
+        }else
+        {
+            res.send('deleted')
         }
-    
-        connection.promise().query('insert into empolyee_details values (?,?,?,?)', [id,name,employee_code,salary])
-        .then(data => res.status(200).send(data[0]))
-        .catch(err => console.log(err))
+    })
     })
 
-app.get('/get', (req,res) =>{
-    connection.promise().query('SELECT * FROM empolyee_details')
-    .then(data => res.send(data[0]))
-    .catch(err => console.log(err))
 
-    })
+app.listen(3002);
+console.log("server is running on port 3002");
 
-    app.listen(3002)
-    console.log('server is running on port 3002')
-
-
-    // async function connect() {
+// async function connect() {
 
 //     try {
 //         const connection = await mysql.createConnection(
